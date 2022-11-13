6a2188aa0f6f95662e9c6b64d9733b39a081367f
siriwardhanasmnp <siriwardhanasmnp.19@uom.lk>
2022-11-13 13:47:08 +0530

    entities added

---
+++ src/main/java/com/havi/inquiry/entity/InquiryOrder.java
@@ -0,0 +1,10 @@
+package com.havi.inquiry.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class InquiryOrder {
+}
---
+++ src/main/java/com/havi/inquiry/entity/InquiryOrderWithTransport.java
@@ -0,0 +1,11 @@
+package com.havi.inquiry.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InquiryOrderWithTransport {
+
+
+}
