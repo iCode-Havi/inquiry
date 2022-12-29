24a125cd9cae4cb274dd64ce9ddf7006920a0790
siriwardhanasmnp <siriwardhanasmnp.19@uom.lk>
2022-12-29 15:01:32 +0530

    attributed added

--- src/main/java/com/havi/inquiry/entity/InquiryOrder.java
+++ src/main/java/com/havi/inquiry/entity/InquiryOrder.java
@@ -1,10 +1,36 @@
 package com.havi.inquiry.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 
+@Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class InquiryOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long farmerId;
+    private String payment;
+    private String paymentStatus;
+    private String address;
+    private LocalDateTime time;
+    private String email;
+    private String customerStatus;
+    private LocalDateTime customerReceiveTime;
+    private Long orderCode;
+    private String farmerGivenStatus;
+    private Long contact;
+    private Integer quantity;
+
 }
--- src/main/java/com/havi/inquiry/entity/InquiryOrderWithTransport.java
+++ src/main/java/com/havi/inquiry/entity/InquiryOrderWithTransport.java
@@ -1,11 +1,21 @@
 package com.havi.inquiry.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-public class InquiryOrderWithTransport {
+@AllArgsConstructor
+@RequiredArgsConstructor
 
-
+public class InquiryOrderWithTransport extends InquiryOrder {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long OrderId;
 }
